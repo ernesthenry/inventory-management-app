c76be2f158eddc0527b23f52bd7dccb5b4cf1b17
osamiull <omarsamiullah1@gmail.com>
2024-08-05 12:22:53 -0700

    I fixed page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,291 +1,30 @@
-// 'use client'
-
-// import { useState, useEffect } from 'react'
-// import { Box, Stack, Typography, Button, Modal, TextField } from '@mui/material'
-// import { firestore } from '@/firebase'
-// import {
-//   collection,
-//   doc,
-//   getDocs,
-//   query,
-//   setDoc,
-//   deleteDoc,
-//   getDoc,
-// } from 'firebase/firestore'
-
-// const colors = {
-//   lightBlue: '#ADD8E6',
-//   darkBlue: '#4682B4',
-//   white: '#FFFFFF',
-//   primaryRed: '#e63946',
-//   dark: '#1d3557',
-//   green: '#4CAF50',
-// };
-
-// const modalStyle = {
-//   position: 'absolute',
-//   top: '50%',
-//   left: '50%',
-//   transform: 'translate(-50%, -50%)',
-//   width: 300,
-//   bgcolor: colors.white,
-//   border: `2px solid ${colors.primaryRed}`,
-//   boxShadow: 24,
-//   p: 4,
-//   borderRadius: '8px',
-//   display: 'flex',
-//   flexDirection: 'column',
-//   gap: 3,
-// }
-
-// export default function Home() {
-//   const [inventory, setInventory] = useState([])
-//   const [open, setOpen] = useState(false)
-//   const [itemName, setItemName] = useState('')
-//   const [searchQuery, setSearchQuery] = useState('')
-
-//   const updateInventory = async () => {
-//     try {
-//       const snapshot = query(collection(firestore, 'inventory'))
-//       const docs = await getDocs(snapshot)
-//       const inventoryList = []
-//       docs.forEach((doc) => {
-//         inventoryList.push({ name: doc.id, ...doc.data() })
-//       })
-//       setInventory(inventoryList)
-//     } catch (error) {
-//       console.error('Error fetching inventory:', error)
-//     }
-//   }
-
-//   useEffect(() => {
-//     updateInventory()
-//   }, [])
-
-//   const addItem = async (item) => {
-//     try {
-//       const docRef = doc(collection(firestore, 'inventory'), item)
-//       const docSnap = await getDoc(docRef)
-//       if (docSnap.exists()) {
-//         const { quantity } = docSnap.data()
-//         await setDoc(docRef, { quantity: quantity + 1 })
-//       } else {
-//         await setDoc(docRef, { quantity: 1 })
-//       }
-//       await updateInventory()
-//     } catch (error) {
-//       console.error('Error adding item:', error)
-//     }
-//   }
-
-//   const removeItem = async (item) => {
-//     try {
-//       const docRef = doc(collection(firestore, 'inventory'), item)
-//       const docSnap = await getDoc(docRef)
-//       if (docSnap.exists()) {
-//         const { quantity } = docSnap.data()
-//         if (quantity === 1) {
-//           await deleteDoc(docRef)
-//         } else {
-//           await setDoc(docRef, { quantity: quantity - 1 })
-//         }
-//       }
-//       await updateInventory()
-//     } catch (error) {
-//       console.error('Error removing item:', error)
-//     }
-//   }
-
-//   const handleOpen = () => setOpen(true)
-//   const handleClose = () => setOpen(false)
-
-//   const handleKeyPress = (event) => {
-//     if (event.key === 'Enter') {
-//       addItem(itemName)
-//       setItemName('')
-//       handleClose()
-//     }
-//   }
-
-//   const filteredInventory = inventory.filter((item) =>
-//     item.name.toLowerCase().includes(searchQuery.toLowerCase())
-//   )
-
-//   return (
-//     <div className="container">
-//       <Modal
-//         open={open}
-//         onClose={handleClose}
-//         aria-labelledby="modal-modal-title"
-//         aria-describedby="modal-modal-description"
-//       >
-//         <Box sx={modalStyle}>
-//           <Typography id="modal-modal-title" variant="h6" component="h2" sx={{ fontSize: '1.3rem' }}>
-//             Add Item
-//           </Typography>
-//           <TextField
-//             id="outlined-basic"
-//             label="Item"
-//             variant="outlined"
-//             fullWidth
-//             value={itemName}
-//             onChange={(e) => setItemName(e.target.value)}
-//             onKeyPress={handleKeyPress}
-//             className="input-field"
-//             InputProps={{ style: { fontSize: '1rem' } }}
-//             InputLabelProps={{ style: { fontSize: '1rem' } }}
-//           />
-//           <Button
-//             variant="contained"
-//             onClick={() => {
-//               addItem(itemName)
-//               setItemName('')
-//               handleClose()
-//             }}
-//             className="btn"
-//             sx={{ fontSize: '1rem', backgroundColor: colors.darkBlue }}
-//           >
-//             Add
-//           </Button>
-//         </Box>
-//       </Modal>
-//       <div className="sub-header" style={{ backgroundColor: colors.primaryRed, padding: '15px', borderRadius: '8px', marginBottom: '20px' }}>
-//         <Typography variant="h3" style={{ fontWeight: 'bold', color: colors.white, textAlign: 'center' }}>
-//           Inventory
-//         </Typography>
-//       </div>
-//       <div className="main-content" style={{ backgroundColor: colors.lightBlue, padding: '20px', borderRadius: '8px', maxWidth: '600px', margin: '0 auto' }}>
-//         <Box
-//           sx={{
-//             backgroundColor: colors.white,
-//             padding: '15px',
-//             borderRadius: '8px',
-//             marginBottom: '20px',
-//             boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
-//             width: '100%',
-//             maxWidth: '500px',
-//             margin: '0 auto',
-//           }}
-//         >
-//           <Button
-//             variant="contained"
-//             onClick={handleOpen}
-//             className="btn"
-//             sx={{ 
-//               marginBottom: '15px', 
-//               fontSize: '1rem',
-//               backgroundColor: colors.darkBlue,
-//               '&:hover': {
-//                 backgroundColor: colors.primaryRed,
-//               },
-//               width: '100%',
-//             }}
-//           >
-//             Add New Item
-//           </Button>
-//           <TextField
-//             fullWidth
-//             variant="outlined"
-//             label="Search Items"
-//             value={searchQuery}
-//             onChange={(e) => setSearchQuery(e.target.value)}
-//             className="input-field"
-//             sx={{ 
-//               '& .MuiOutlinedInput-root': {
-//                 '& fieldset': {
-//                   borderColor: colors.lightBlue,
-//                 },
-//                 '&:hover fieldset': {
-//                   borderColor: colors.darkBlue,
-//                 },
-//                 '&.Mui-focused fieldset': {
-//                   borderColor: colors.darkBlue,
-//                 },
-//               },
-//               '& .MuiInputBase-input': {
-//                 fontSize: '0.85rem',
-//                 padding: '10px 14px',
-//               },
-//               '& .MuiInputLabel-root': {
-//                 fontSize: '0.85rem',
-//               },
-//             }}
-//           />
-//         </Box>
-//         <ul className="item-list" style={{ padding: 0, margin: '20px 0 0 0' }}>
-//           {filteredInventory.map(({ name, quantity }) => (
-//             <li key={name} className="item" style={{ 
-//               backgroundColor: colors.white,
-//               borderRadius: '8px',
-//               padding: '12px',
-//               marginBottom: '10px',
-//               display: 'flex',
-//               justifyContent: 'space-between',
-//               alignItems: 'center',
-//               boxShadow: '0 2px 4px rgba(0,0,0,0.05)',
-//               width: '100%',
-//               maxWidth: '500px',
-//               margin: '0 auto 10px auto',
-//             }}>
-//               <Typography variant="h6" color={colors.dark} style={{ fontWeight: 'bold', fontSize: '1.2rem' }}>
-//                 {name.charAt(0).toUpperCase() + name.slice(1)}
-//               </Typography>
-//               <Typography variant="body1" color={colors.dark} style={{ fontSize: '1rem' }}>
-//                 Quantity: {quantity}
-//               </Typography>
-//               <Button 
-//                 variant="contained" 
-//                 onClick={() => removeItem(name)} 
-//                 className="btn-remove" 
-//                 sx={{ 
-//                   fontSize: '0.8rem',
-//                   backgroundColor: colors.primaryRed,
-//                   '&:hover': {
-//                     backgroundColor: colors.darkBlue,
-//                   },
-//                 }}
-//               >
-//                 Remove
-//               </Button>
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     </div>
-//   )
-// }
-
-'use client'
+'use client';
 
 import React, { useState, useEffect } from 'react'
 import { Box, Stack, Typography, Button, Modal, TextField } from '@mui/material'
 import { firestore, storage } from '@/firebase'
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
 import {
-  collection, doc, getDocs, query, setDoc, deleteDoc, getDoc,
+  collection,
+  doc,
+  getDocs,
+  query,
+  setDoc,
+  deleteDoc,
+  getDoc,
 } from 'firebase/firestore'
 import ImageUploading from 'react-images-uploading'
 
-const colors = {
-  lightBlue: '#ADD8E6',
-  darkBlue: '#4682B4',
-  white: '#FFFFFF',
-  primaryRed: '#e63946',
-  dark: '#1d3557',
-  green: '#4CAF50',
-};
-
-const modalStyle = {
+const style = {
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 300,
-  bgcolor: colors.white,
-  border: `2px solid ${colors.primaryRed}`,
+  width: 400,
+  bgcolor: 'white',
+  border: '2px solid #000',
   boxShadow: 24,
   p: 4,
-  borderRadius: '8px',
   display: 'flex',
   flexDirection: 'column',
   gap: 3,
@@ -295,6 +34,27 @@ export default function Home() {
   const [inventory, setInventory] = useState([])
   const [open, setOpen] = useState(false)
   const [itemName, setItemName] = useState('')
+  const [searchQuery, setSearchQuery] = useState('')
+  const [images, setImages] = useState([])
+  const maxNumber = 69
+
+  const handleImageChange = (imageList, addUpdateIndex) => {
+    setImages(imageList)
+  }
+
+  const uploadImageToFirebase = async (file) => {
+    const storageRef = ref(storage, `images/${file.name}`)
+    await uploadBytes(storageRef, file)
+    return await getDownloadURL(storageRef)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (images.length === 0) return
+    const imageUrl = await uploadImageToFirebase(images[0].file)
+    console.log('Uploaded image URL:', imageUrl)
+    // Call your image analysis API here with imageUrl
+  }
 
   const updateInventory = async () => {
     try {
@@ -309,6 +69,10 @@ export default function Home() {
       console.error('Error fetching inventory:', error)
     }
   }
+
+  useEffect(() => {
+    updateInventory()
+  }, [])
 
   const addItem = async (item) => {
     try {
@@ -347,89 +111,152 @@ export default function Home() {
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
 
+  const filteredInventory = inventory.filter((item) =>
+    item.name.toLowerCase().includes(searchQuery.toLowerCase())
+  )
+
   return (
-    <Box
-      width="100vw"
-      height="100vh"
-      display="flex"
-      justifyContent="center"
-      flexDirection="column"
-      alignItems="center"
-      gap={2}
-    >
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+    <>
+      <main className="container mt-6 mx-auto px-4 max-w-2xl flex flex-col gap-4">
+        <h1 className="text-4xl font-bold">GPT-4 Image Analyzer</h1>
+
+        <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+          <ImageUploading
+            multiple
+            value={images}
+            onChange={handleImageChange}
+            maxNumber={maxNumber}
+            dataURLKey="data_url"
+          >
+            {({
+              imageList,
+              onImageUpload,
+              onImageRemoveAll,
+              onImageUpdate,
+              onImageRemove,
+              isDragging,
+              dragProps,
+            }) => (
+              <div className="upload__image-wrapper">
+                <button
+                  style={isDragging ? { color: 'red' } : undefined}
+                  onClick={onImageUpload}
+                  {...dragProps}
+                >
+                  Click or Drop here
+                </button>
+                &nbsp;
+                <button onClick={onImageRemoveAll}>Remove all images</button>
+                {imageList.map((image, index) => (
+                  <div key={index} className="image-item">
+                    <img src={image['data_url']} alt="" width="100" />
+                    <div className="image-item__btn-wrapper">
+                      <button onClick={() => onImageUpdate(index)}>Update</button>
+                      <button onClick={() => onImageRemove(index)}>Remove</button>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </ImageUploading>
+          <button className="uppercase py-2 px-2 rounded-xl bg-lime-700 text-white">
+            Analyze
+          </button>
+        </form>
+      </main>
+
+      <Box
+        width="100vw"
+        height="100vh"
+        display="flex"
+        justifyContent="center"
+        flexDirection="column"
+        alignItems="center"
+        gap={2}
       >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Add Item
-          </Typography>
-          <Stack width="100%" direction="row" spacing={2}>
-            <TextField
-              id="outlined-basic"
-              label="Item"
-              variant="outlined"
-              fullWidth
-              value={itemName}
-              onChange={(e) => setItemName(e.target.value)}
-            />
-            <Button
-              variant="outlined"
-              onClick={() => {
-                addItem(itemName)
-                setItemName('')
-                handleClose()
-              }}
-            >
-              Add
-            </Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Add Item
+            </Typography>
+            <Stack width="0%" direction="row" spacing={2}>
+              <TextField
+                id="outlined-basic"
+                label="Item"
+                variant="outlined"
+                fullWidth
+                value={itemName}
+                onChange={(e) => setItemName(e.target.value)}
+              />
+              <Button
+                variant="outlined"
+                onClick={() => {
+                  addItem(itemName)
+                  setItemName('')
+                  handleClose()
+                }}
+              >
+                Add
+              </Button>
+            </Stack>
+          </Box>
+        </Modal>
+        <Button variant="contained" onClick={handleOpen}>
+          Add New Item
+        </Button>
+        <TextField
+          width="800px"
+
+          label="Search"
+          variant="outlined"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          style={{ marginBottom: '1rem' }}
+        />
+        <Box border="1px solid #333">
+          <Box
+            width="800px"
+            height="100px"
+            bgcolor="#ADD8E6"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Typography variant="h2" color="#333" textAlign="center">
+              Inventory Items
+            </Typography>
+          </Box>
+          <Stack width="800px" height="300px" spacing={2} overflow="auto">
+            {filteredInventory.map(({ name, quantity }) => (
+              <Box
+                key={name}
+                width="100%"
+                minHeight="150px"
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                bgcolor="#f0f0f0"
+                paddingX={5}
+              >
+                <Typography variant="h3" color="#333" textAlign="center">
+                  {name.charAt(0).toUpperCase() + name.slice(1)}
+                </Typography>
+                <Typography variant="h3" color="#333" textAlign="center">
+                  Quantity: {quantity}
+                </Typography>
+                <Button variant="contained" onClick={() => removeItem(name)}>
+                  Remove
+                </Button>
+              </Box>
+            ))}
           </Stack>
         </Box>
-      </Modal>
-      <Button variant="contained" onClick={handleOpen}>
-        Add New Item
-      </Button>
-      <Box border="1px solid #333">
-        <Box
-          width="800px"
-          height="100px"
-          bgcolor="#ADD8E6"
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Typography variant="h2" color="#333" textAlign="center">
-            Inventory Items
-          </Typography>
-        </Box>
-        <Stack width="800px" height="300px" spacing={2} overflow="auto">
-          {inventory.map(({ name, quantity }) => (
-            <Box
-              key={name}
-              width="100%"
-              minHeight="150px"
-              display="flex"
-              justifyContent="space-between"
-              alignItems="center"
-              bgcolor="#f0f0f0"
-              paddingX={5}
-            >
-              <Typography variant="h3" color="#333" textAlign="center">
-                {name.charAt(0).toUpperCase() + name.slice(1)}
-              </Typography>
-              <Typography variant="h3" color="#333" textAlign="center">
-                Quantity: {quantity}
-              </Typography>
-              <Button variant="contained" onClick={() => removeItem(name)}>
-                Remove
-              </Button>
-            </Box>
-          ))}
-        </Stack>
       </Box>
-    </Box>
+    </>
   )
 }
--- src/firebase.js
+++ src/firebase.js
@@ -23,13 +23,13 @@ import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBmz4L994uktn648GXNSvFNNemHchSk24g",
-  authDomain: "inventory-management-app-d7d36.firebaseapp.com",
-  projectId: "inventory-management-app-d7d36",
-  storageBucket: "inventory-management-app-d7d36.appspot.com",
-  messagingSenderId: "859834793201",
-  appId: "1:859834793201:web:549daec6ef231be37b7d50",
-  measurementId: "G-VBMV5PKVCQ"
+  apiKey: "AIzaSyBgYK1IT7xwrmR93t0sOerybFVhqH3faTk",
+  authDomain: "inventory-management-d9e7b.firebaseapp.com",
+  projectId: "inventory-management-d9e7b",
+  storageBucket: "inventory-management-d9e7b.appspot.com",
+  messagingSenderId: "733036480925",
+  appId: "1:733036480925:web:b01867a377f13ed990c774",
+  measurementId: "G-2GBRV5WH3H"
 };
 
 let app;
