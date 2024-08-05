723fb94469dd736838019ec62f0e79b5c58fea72
osamiull <omarsamiullah1@gmail.com>
2024-08-05 16:08:02 -0700

    I made the layout look better

--- src/firebase.js
+++ src/firebase.js
@@ -1,26 +1,6 @@
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import { getFirestore } from 'firebase/firestore';
-
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBmz4L994uktn648GXNSvFNNemHchSk24g",
-//   authDomain: "inventory-management-app-d7d36.firebaseapp.com",
-//   projectId: "inventory-management-app-d7d36",
-//   storageBucket: "inventory-management-app-d7d36.appspot.com",
-//   messagingSenderId: "859834793201",
-//   appId: "1:859834793201:web:549daec6ef231be37b7d50",
-//   measurementId: "G-VBMV5PKVCQ"
-// };
-
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-// const firestore = getFirestore(app);
-// export { firestore, analytics };
-
-
 import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage'; // Add this import
 
 const firebaseConfig = {
   apiKey: "AIzaSyBgYK1IT7xwrmR93t0sOerybFVhqH3faTk",
@@ -35,10 +15,12 @@ const firebaseConfig = {
 let app;
 let firestore;
 let analytics;
+let storage; // Add this line
 
 if (typeof window !== 'undefined' && !getApps().length) {
   app = initializeApp(firebaseConfig);
   firestore = getFirestore(app);
+  storage = getStorage(app); // Initialize storage
   
   // Dynamically import analytics to avoid SSR issues
   import('firebase/analytics').then((module) => {
@@ -46,4 +28,4 @@ if (typeof window !== 'undefined' && !getApps().length) {
   });
 }
 
-export { firestore, analytics };
+export { firestore, analytics, storage } // Export storage
